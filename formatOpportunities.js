054537d86204a251ddf18332f29b449eace29376
Michael J. Flynn <flynnmj@devnoise.com>
2015-11-25 09:11:23 -0500

    Error Catching
    Added usage output if filename is not given. Put some try/catch
    statements in to make the errors more readable. Still need to figure
    out better error handling if the file is read only.

--- formatOpportunities.js
+++ formatOpportunities.js
@@ -1,12 +1,32 @@
 var fs = require('fs');
 
 var fileName = process.argv[2];
-var text = fs.readFileSync(fileName, "utf8");
-var newFileName = fileName.split(".",1) + ".new.json";
 
-var opportunties = JSON.parse(text);
+if (fileName) {
+	try {
+		var text = fs.readFileSync(fileName, "utf8");
+		var opportunties = JSON.parse(text);
+	} catch (e) {
+		console.log("ERROR: " + e.message);
+		process.exit(1);
+	}
+	
+	try {
+		var out = fs.createWriteStream(fileName, { encoding: "utf8" });
+		out.write(JSON.stringify(opportunties, null, 2));
+		out.end(); 
+	} catch (e) {
+		console.log("ERROR: Couldn't write content out to: " + fileName + "");
+		console.log("ERROR: " + e.message);
+		process.exit(1);
+	}
+} else {
+	function getFileName(path) {
+		return path.substring(path.lastIndexOf('/') + 1, path.length);
+	}
+	var node = getFileName(process.argv[0]);
 
-var out = fs.createWriteStream(fileName, { encoding: "utf8" });
-out.write(JSON.stringify(opportunties, null, 2));
-out.end(); 
+	var script = getFileName(process.argv[1]);
 
+	console.log("Usage: " + node + " " + script + " <filename>");
+}
