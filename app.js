8794b05fe58937f1e1508cf02b8811e97998cf96
ramjac <ramjacvp@gmail.com>
2015-12-20 08:42:07 -0500

    Added css and made changes to the html to use the css.
    Also editted the js because I was getting a 404.

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ angular.module('jobapp', [])
 
 
 .controller('mycontroller', function($scope, $http) {
-  $http.get('https://raw.githubusercontent.com/mcelaney/opportunities/master/philadelphia.json').success(function(data) {
+    var contentUrl ='https://raw.githubusercontent.com/mcelaney/opportunities/master/pa-philadelphia.json';
+    $http.get(contentUrl).success(function(data) {
       $scope.listings = data;
   });
 });
