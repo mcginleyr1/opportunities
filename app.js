0171f27af267472671fadf377f6b83417dd46125
Francis Bongiovanni <fbongiovanni29@gmail.com>
2016-01-23 00:37:36 -0500

    search feature added

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 angular.module('jobapp', [])
 .controller('mycontroller', function($scope, $http) {
   $http.get('https://raw.githubusercontent.com/mcelaney/opportunities/master/all-companies.json').success(function(data) {
-      $scope.listings = data;
+    $scope.listings = data;
   });
 });
