35682abff939bd0cd7fdf9a9b663e530d9ee75f1
Francis Bongiovanni <fbongiovanni29@gmail.com>
2016-01-27 12:10:45 -0500

    empty title in AgileTrailblazers

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 angular.module('jobapp', [])
 .controller('mycontroller', function($scope, $http) {
-  $http.get('https://raw.githubusercontent.com/mcelaney/opportunities/master/all-companies.json').success(function(data) {
+  $http.get('https://raw.githubusercontent.com/fbongiovanni29/opportunities/web-app/all-companies.json').success(function(data) {
     $scope.listings = data;
   });
 });
